f86821228afee0691c65cc2ae786b847e4b16df0
alananigeorge <alanani43@gmail.com>
2026-02-11 13:39:39 +0530

    Add files via upload

---
+++ alan.py
@@ -0,0 +1,14 @@
+print("Alan is learning Git")
+print ("Alan should beat scolosis")
+print ('body building')
+print('Make  lakh and disapper')
+"+-*/"
+
+a = 10
+b = 20
+
+print("The sum of a and b is", a + b)
+
+print("The difference of a and b is", a - b)
+print("The product of a and b is", a * b)
+print("The quotient of a and b is", a / b)
